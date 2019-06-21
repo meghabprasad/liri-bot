d4facd317829fc521e1e3d2eb2a0450ba4379c89
Megha Prasad Bindiganavale <meghab.prasad@gmail.com>
2019-06-20 22:35:24 -0700

    use inquirer to get user input

--- liri.js
+++ liri.js
@@ -1,3 +1,14 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
+var inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "task",
+        message: "What would you like to do?",
+    }
+]).then(function(inquirerResponse){
+    console.log(inquirerResponse.task);
+})
